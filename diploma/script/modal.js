f5198bc76f81ae801643e74a0fed1a9b70338506
zanik88 <68947293+zanik88@users.noreply.github.com>
2021-12-22 22:44:55 +0400

    Add files via upload

--- diploma/script/modal.js
+++ diploma/script/modal.js
@@ -3,8 +3,8 @@ if($(e.target).closest('.modal').length ==0){
     $(this).fadeOut()
 }
 })
-$('.close').on('click', function (){
-    $(this).parents(.'modal-overlay').fadeOut();
+$('.close').on('click', function(){
+    $(this).parents('.modal-overlay').fadeOut();
 })
 $('#modal-button').on('click', function(){
   $('.modal-overlay').fadeIn();
